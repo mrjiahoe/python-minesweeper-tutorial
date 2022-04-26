9484027dedcbd082b5a70d460b1e2876febf0064
jiahoe <mrjiahoe@hotmail.com>
2022-04-26 12:39:31 +0800

    create cells

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils
-
 
 root = Tk()
 # Override the settings of the window
@@ -33,6 +33,15 @@ center_frame = Frame(
     height=utils.height_prct(75)
 )
 center_frame.place(x=utils.width_prct(25),y=utils.height_prct(25))
+
+for x in range(settings.GRID_SIZE) :
+    for y in range(settings.GRID_SIZE) :
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x,
+            row=y
+        )
 
 # Run the window
 root.mainloop()
