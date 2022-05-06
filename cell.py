bedc193257a4dfac7f07d5c3f3c3ba2934ec5df0
jiahoe <mrjiahoe@hotmail.com>
2022-05-06 23:49:01 +0800

    show remaining cell count

--- cell.py
+++ cell.py
@@ -1,41 +1,58 @@
-from tkinter import Button
+from tkinter import Button, Label
 import random
 import settings
 
+
 class Cell:
     all = []
-    
-    def __init__(self,x,y, is_mine=False):
+
+    cell_count = settings.CELL_COUNT
+    cell_count_label_object = None
+
+    def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
+        self.is_opened = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
-        
+
         # Append the object to the Cell.all list
         Cell.all.append(self)
-        
-    def create_btn_object(self,location):
+
+    def create_btn_object(self, location):
         btn = Button(
             location,
             width=12,
             height=4,
         )
-        btn.bind('<Button-1>', self.left_click_actions) # Left Click
-        btn.bind('<Button-3>', self.right_click_actions) # Right Click
+        btn.bind('<Button-1>', self.left_click_actions)  # Left Click
+        btn.bind('<Button-3>', self.right_click_actions)  # Right Click
         self.cell_btn_object = btn
-        
+
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(location,
+                    bg='black',
+                    fg='white',
+                    text=f"Cells Left:{Cell.cell_count}",
+                    font=("", 30))
+        Cell.cell_count_label_object = lbl
+
     def left_click_actions(self, event):
         if self.is_mine:
             self.show_mine()
         else:
+            if self.surrounded_cells_mines_length == 0:  # if nearby cell has 0 mines, show number of mines of nearby cell
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
             self.show_cell()
-            
-    def get_cell_by_axis(self, x,y):
+
+    def get_cell_by_axis(self, x, y):
         # Return cell object based on the value of x, y
         for cell in Cell.all:
             if cell.x == x and cell.y == y:
                 return cell
-            
+
     @property
     def surrounded_cells(self):
         cells = [
@@ -50,7 +67,7 @@ class Cell:
         ]
         cells = [cell for cell in cells if cell is not None]
         return cells
-    
+
     @property
     def surrounded_cells_mines_length(self):
         counter = 0
@@ -58,25 +75,32 @@ class Cell:
             if cell.is_mine:
                 counter += 1
         return counter
-    
+
     def show_cell(self):
-        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
-            
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(
+                text=self.surrounded_cells_mines_length)
+            # Replace the text of cell count label with the newer count
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f"Cells Left:{Cell.cell_count}")
+        # Mark the cell as opened (use this as the last line of this method)
+        self.is_opened = True
+
     def show_mine(self):
         # A logic to interrupt the game and display a message that player lost!
         self.cell_btn_object.configure(bg='red')
-        
+
     def right_click_actions(self, event):
         print(event)
         print("I am right clicked!")
-        
+
     @staticmethod
     def randomize_mines():
-        picked_cells = random.sample(
-            Cell.all, settings.MINES_COUNT
-        )
+        picked_cells = random.sample(Cell.all, settings.MINES_COUNT)
         for picked_cells in picked_cells:
             picked_cells.is_mine = True
-    
+
     def __repr__(self):
         return f"Cell({self.x}, {self.y})"
--- main.py
+++ main.py
@@ -10,39 +10,34 @@ root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title('Minesweeper Clone')
 root.resizable(False, False)
 
-top_frame = Frame(
-    root, 
-    bg='black', 
-    width=utils.width_prct(100),
-    height=utils.height_prct(25)
-)
-top_frame.place(x=0,y=0)
+top_frame = Frame(root,
+                  bg='black',
+                  width=utils.width_prct(100),
+                  height=utils.height_prct(25))
+top_frame.place(x=0, y=0)
 
-left_frame = Frame(
-    root,
-    bg='black',
-    width=utils.width_prct(25),
-    height=utils.height_prct(75)
-)
-left_frame.place(x=0,y=utils.height_prct(25))
+left_frame = Frame(root,
+                   bg='black',
+                   width=utils.width_prct(25),
+                   height=utils.height_prct(75))
+left_frame.place(x=0, y=utils.height_prct(25))
 
-center_frame = Frame(
-    root,
-    bg="black",
-    width=utils.width_prct(75),
-    height=utils.height_prct(75)
-)
-center_frame.place(x=utils.width_prct(25),y=utils.height_prct(25))
+center_frame = Frame(root,
+                     bg="black",
+                     width=utils.width_prct(75),
+                     height=utils.height_prct(75))
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
-for x in range(settings.GRID_SIZE) :
-    for y in range(settings.GRID_SIZE) :
-        c = Cell(x,y)
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell(x, y)
         c.create_btn_object(center_frame)
-        c.cell_btn_object.grid(
-            column=x,
-            row=y
-        )
-        
+        c.cell_btn_object.grid(column=x, row=y)
+
+# Call the label from the Cell class
+Cell.create_cell_count_label(left_frame)
+Cell.cell_count_label_object.place(x=0, y=0)
+
 Cell.randomize_mines()
 
 # Run the window
